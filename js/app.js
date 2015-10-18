7bf2b7d17f8386fc354a7f24df6a15931c344bf0
Tugce Sirin <ztugcesirin@gmail.com>
2015-10-18 20:32:36 +0300

    routing çalışması

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module("angularProje", []);
+var app = angular.module("angularProje", ['ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/AddBilgi', {
+    templateUrl: 'add-bilgi.html',
+    controller: 'AddBilgiController'
+  })
+  .otherwise('/', {
+    templateUrl: 'index.html',
+    controller: 'MainController'
+  });
+
+});
--- js/controllers/AddBilgiController.js
+++ js/controllers/AddBilgiController.js
@@ -1,3 +1,4 @@
 app.controller('AddBilgiController', function($scope) {
     $scope.message = 'This is Add new bilgi screen';
+    $scope.params = $routeParams;
 });
