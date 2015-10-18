12d40179d55e79397503f3182e0c35532160786a
Tugce Sirin <ztugcesirin@gmail.com>
2015-10-18 22:42:06 +0300

    input alanları eklendi.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,15 +13,12 @@ app.controller('MainController', ['$scope', function($scope, $route, $routeParam
 	$scope.yeniBilgiNo = "->";
 	$scope.yeniBilgiAd = "";
 	$scope.icerik = "";
-	$scope.yeniSenaryo = function(){
+	$scope.yeniBilgiEkle = function(){
 			var yeniBilgi = [{isim: $scope.yeniBilgiAd, no: $scope.bilgi.length + 1, icerik: icerik}];
 			var currentList = $scope.bilgi;
 			var newList = currentList.concat(yeniBilgi);
 			$scope.bilgi = newList;
 			yeniBilgiNo = yeniBilgiNo + 1;
 	};
-	$scope.go = function ( hash ) {
-  $location.path( hash );
-};
 
 }]);
